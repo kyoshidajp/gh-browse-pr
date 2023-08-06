703ac40e2d5730b376f1138f2bbbbea9376aef76
Katsuhiko YOSHIDA <claddvd@gmail.com>
2023-08-06 17:26:24 +0900

    Add tests

--- main_test.go
+++ main_test.go
@@ -15,3 +15,18 @@ func TestGetPrUrl(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func TestGetNewPrUrl(t *testing.T) {
+	assert.Equal(t,
+		"https://github.com/kyoshidajp/gh-browse-pr/compare/test?expand=1",
+		GetNewPrUrl("https://github.com/kyoshidajp/gh-browse-pr", "test"),
+	)
+}
+
+func TestIsNumberString(t *testing.T) {
+	assert.Equal(t, true, IsNumberString("100"))
+	assert.Equal(t, true, IsNumberString("001"))
+	assert.Equal(t, true, IsNumberString("-1")) // allow
+	assert.Equal(t, false, IsNumberString("test"))
+	assert.Equal(t, false, IsNumberString("1test"))
+}
